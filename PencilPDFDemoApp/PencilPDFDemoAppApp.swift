97419e55136647f4f212e1ea317f8a5801a33485
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2024-04-27 14:38:01 +0900

    프로젝트 생성

---
+++ PencilPDFDemoApp/PencilPDFDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilPDFDemoAppApp.swift
+//  PencilPDFDemoApp
+//
+//  Created by Zerom on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct PencilPDFDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
